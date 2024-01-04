b997a5ea863056b69fcd908ecba107b42c1b1c50
centawang <centawy@outlook.com>
2024-01-04 13:55:14 +0800

    modified:   keyboards/crkbd/keymaps/vial/keymap.c 	modified:   keyboards/crkbd/keymaps/vial/rules.mk 	modified:   keyboards/crkbd/rev1/config.h

--- keyboards/crkbd/rev1/config.h
+++ keyboards/crkbd/rev1/config.h
@@ -24,3 +24,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
         { 27, 27 }
 #    define SPLIT_TRANSPORT_MIRROR
 #endif
+
+#define SPLIT_MODS_ENABLE
+#define SPLIT_LED_STATE_ENABLE
+#define SPLIT_LAYER_STATE_ENABLE
+
+
